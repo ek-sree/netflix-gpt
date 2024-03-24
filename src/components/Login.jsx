05d4fcf026483304c097db38a92761a0b2d0e1d4
ek-sree <eksreehari05@gmail.com>
2024-03-24 14:46:42 +0530

    config firebase

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import Header from "./Header";
 import { CheckvalidateData } from "../utils/validate";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../utils/firebase";
 
 
@@ -36,6 +36,16 @@ const Login = () => {
           seterrorMessage(errorCode+" - "+errorMessage)
         });
     }else{
+        signInWithEmailAndPassword(auth, email.current.value,password.current.value)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          seterrorMessage(errorCode+" - "+errorMessage)
+        });
       
     }
 
